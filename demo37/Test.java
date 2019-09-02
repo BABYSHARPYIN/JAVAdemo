9e172296f8201b8958fe2d498742f3adc86743db
yinrui <34263845+BABYSHARPYIN@users.noreply.github.com>
2019-09-02 18:33:45 +0800

    讲解集合中。。。但是我现在状态不是很好，快下班了date();一下。。。148集，希望明天继续学习的时候能记起来

---
+++ demo37/Test.java
@@ -0,0 +1,21 @@
+package cn.itcast.demo37;
+/*
+    需求：向List集合中添加三个元素，并遍历打印
+
+    分析：
+    1、向集合中添加元素的方法为：add()
+    2、遍历集合的方式：for循环
+    3、获取集合中元素个数的方式：size()
+
+    步骤：
+    1、创建集合对象：
+        List list = new ArrayList();
+    2、分别创建三个Student对象
+    3、使用add方法将Student对象添加到集合中
+    4、使用for循环遍历集合并打印
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
